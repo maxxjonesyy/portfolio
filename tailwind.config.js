a1bf4480e7b06ac34d15ea97a0287a88d5ef8a0f
Maxwell Jones <maxxjonesyy@gmail.com>
2023-04-13 14:05:41 +1000

    add dark theme and theme presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkText: "#EFECEC",
+        darkBg: "#262626",
+        lightText: "#545454",
+        lightBg: "#f5f5f5",
+        accent: "#8A61FF",
+      },
+    },
   },
   plugins: [],
 };
