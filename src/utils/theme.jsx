8f69cd1fc6f0ab276ba9463d38c555f06cdc91af
Maxwell Jones <maxxjonesyy@gmail.com>
2023-04-14 13:05:13 +1000

    fix light theme background not being set

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -1,11 +1,13 @@
 function setInitialTheme(theme) {
   if (theme === "dark") {
     document.documentElement.classList.add("dark");
+    document.documentElement.classList.remove("bg-lightBg");
     document.documentElement.classList.add("bg-darkBg");
     localStorage.theme = "dark";
   } else {
     document.documentElement.classList.remove("dark");
     document.documentElement.classList.remove("bg-darkBg");
+    document.documentElement.classList.add("bg-lightBg");
     localStorage.theme = "light";
   }
 }
