2828e7cd08d3276ffb3beb625f3ea20f310db558
Maxwell Jones <maxxjonesyy@gmail.com>
2023-05-11 17:21:03 +1000

    add ability to hide image on skill tile component

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-function Tile({ skill }) {
+function Tile({ skill, img }) {
   return (
     <div className='flex gap-1.5 p-2 rounded-md shadow-sm bg-lightTile dark:bg-darkTile'>
-      <img
-        src={`icons/${skill.toLowerCase()}.svg`}
-        alt={skill}
-        className='w-4'
-      />
-      <p className='text-sm text-lightText dark:text-darkText md:text-base'>
+      {img === true ? (
+        <img
+          src={`icons/${skill.toLowerCase()}.svg`}
+          alt={skill}
+          className='w-4'
+        />
+      ) : null}
+      <p className='text-sm text-lightText dark:text-darkText lg:text-base'>
         {skill}
       </p>
     </div>
