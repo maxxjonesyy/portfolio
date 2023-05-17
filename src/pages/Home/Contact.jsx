fbb0529cec12e25a3780608cb11c5f7b264617ca
Maxwell Jones <maxxjonesyy@gmail.com>
2023-05-17 23:13:53 +1000

    add contact alert for success

--- src/pages/Home/Contact.jsx
+++ src/pages/Home/Contact.jsx
@@ -5,6 +5,8 @@ import Links from "../../components/Links";
 function Contact({ theme }) {
   const form = useRef();
 
+  function clearInput() {}
+
   function sendEmail(e) {
     e.preventDefault();
 
@@ -15,10 +17,19 @@ function Contact({ theme }) {
         form.current,
         "iwCFiT7FYv44pEooo"
       )
-      .then((result) => console.log(result.text)),
-      (error) => {
-        console.log(error.text);
-      };
+      .then(() => {
+        document
+          .querySelectorAll("input")
+          .forEach((input) => (input.value = ""));
+        document.getElementsByTagName("textarea").message.value = "";
+        document.getElementById("send-button").classList.toggle("hidden");
+        document.getElementById("alert").classList.toggle("hidden");
+
+        setTimeout(() => {
+          document.getElementById("send-button").classList.toggle("hidden");
+          document.getElementById("alert").classList.toggle("hidden");
+        }, 5000);
+      });
   }
 
   return (
@@ -60,27 +71,53 @@ function Contact({ theme }) {
           name='user_name'
           placeholder='Name'
           required
-          className='w-full p-2 bg-transparent border-b-2 outline-none md:w-2/3 placeholder-lightText dark:placeholder-darkText border-gray dark:text-darkText text-lightText'
+          className='w-full p-2 transition-all bg-transparent border-b-2 outline-none md:w-2/3 placeholder-lightText dark:placeholder-darkText border-gray dark:text-darkText text-lightText'
         />
         <input
           type='email'
           name='user_email'
           placeholder='Email'
           required
-          className='w-full p-2 bg-transparent border-b-2 outline-none md:w-2/3 placeholder-lightText dark:placeholder-darkText border-gray dark:text-darkText text-lightText'
+          className='w-full p-2 transition-all bg-transparent border-b-2 outline-none md:w-2/3 placeholder-lightText dark:placeholder-darkText border-gray dark:text-darkText text-lightText'
         />
         <textarea
           name='message'
           placeholder='Message'
           required
-          className='w-full p-2 bg-transparent border-b-2 outline-none md:w-2/3 placeholder-lightText dark:placeholder-darkText border-gray dark:text-darkText text-lightText'
+          className='w-full p-2 transition-all bg-transparent border-b-2 outline-none md:w-2/3 placeholder-lightText dark:placeholder-darkText border-gray dark:text-darkText text-lightText'
         />
         <button
+          id='send-button'
           type='submit'
-          className='w-full p-2 text-sm text-white transition duration-300 ease-in-out rounded-md shadow-md md:w-2/3 hover:cursor-pointer bg-accent'
+          className='w-full p-2 text-sm text-white transition-all duration-300 ease-in-out rounded-md shadow-md md:w-2/3 hover:cursor-pointer bg-accent'
         >
           Send
         </button>
+
+        <div
+          id='alert'
+          className='relative flex-col hidden w-full p-2 bg-white rounded-md shadow sm:flex-row sm:items-center md:w-2/3 sm:pr-6'
+        >
+          <div className='flex flex-row items-center justify-center w-full'>
+            <div className='text-green-500'>
+              <svg
+                className='w-6 h-6 sm:w-5 sm:h-5'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+                ></path>
+              </svg>
+            </div>
+            <div className='ml-3 text-sm font-medium'>Email has been sent!</div>
+          </div>
+        </div>
       </form>
     </div>
   );
