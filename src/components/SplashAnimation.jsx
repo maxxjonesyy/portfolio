b1042e267d073e747af8114c1e83cfa2cc4ed845
Maxwell Jones <maxxjonesyy@gmail.com>
2023-05-17 18:57:43 +1000

    change splash bg to theme

--- src/components/SplashAnimation.jsx
+++ src/components/SplashAnimation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SplashAnimation() {
   return (
-    <div className='absolute flex flex-col gap-10 items-center justify-center h-screen w-screen bg-white z-[999] animate-fade-out'>
+    <div className='absolute flex flex-col gap-10 items-center justify-center h-screen w-screen bg-lightBg dark:bg-darkBg z-[999] animate-fade-out'>
       <div className='animate-bounce'>
         <img src='/favicon.svg' alt='splash screen art' className='w-28' />
         <h1 className='pt-2 text-lg text-gray-500'>Loading Projects.</h1>
