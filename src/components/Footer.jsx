932e11cc997fd5797614cfd12132bf6cc29226fb
Maxwell Jones <maxxjonesyy@gmail.com>
2023-05-18 00:29:49 +1000

    footer style changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className='pb-3 font-light text-center text-lightText dark:text-darkText'>
+    <footer className='py-10 font-light text-center opacity-50 text-lightText dark:text-darkText'>
       Designed and built by Maxwell Jones
     </footer>
   );
