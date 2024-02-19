b7e166ef8c8f1a1e1307c95b4adefbcb46ce14a5
Max Jones <maxxjonesyy@gmail.com>
2024-02-19 22:09:21 +1100

    setup routing for views

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home, Projects, Contact } from "./views/index";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
