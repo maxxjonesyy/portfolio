02cd7d036b554e025727ecbe486494f50f5481e7
Max Jones <maxxjonesyy@gmail.com>
2024-02-19 22:13:26 +1100

    add about page and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Projects, Contact } from "./views/index";
+import { Home, About, Projects, Contact } from "./views/index";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
