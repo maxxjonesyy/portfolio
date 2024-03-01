ca6947923593ade38d68c4cc0c28b2398d6b023a
Max Jones <maxxjonesyy@gmail.com>
2024-03-02 08:06:05 +1100

    make variables for transition duration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,12 +10,12 @@ function Footer() {
     <footer className='w-full absolute inline-flex items-center justify-between bottom-0 p-5 dark:text-darkText'>
       <p className='text-body'>Maxwell Jones</p>
       <ul className='inline-flex gap-3'>
-        <li className='w-[1.35rem] md:w-[1.75rem] transition-all'>
+        <li className='w-[1.35rem] md:w-[1.75rem] transition-theme'>
           <a href='https://github.com/maxxjonesyy' target='_blank'>
             <img src={isDark ? GithubDark : Github} alt='Github' />
           </a>
         </li>
-        <li className='w-[1.35rem] md:w-[1.75rem] transition-all'>
+        <li className='w-[1.35rem] md:w-[1.75rem] transition-theme'>
           <a href='https://www.linkedin.com/in/maxxjonesyy/' target='_blank'>
             <img src={isDark ? LinkedInDark : LinkedIn} alt='LinkedIn' />
           </a>
