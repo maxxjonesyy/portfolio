a873c1a639740e36ea050d1416a0c5133f709ed1
Max Jones <maxxjonesyy@gmail.com>
2024-03-07 22:22:55 +1100

    load font and typography theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
       colors: {
         darkText: "#e5e7eb",
         darkBg: "#00171F",
