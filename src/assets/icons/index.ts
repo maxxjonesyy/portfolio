011419a7cc1a9952c0bc687543968761e9925925
Max Jones <maxxjonesyy@gmail.com>
2024-03-19 17:59:10 +1100

    merge index.ts files from assets into one

--- src/assets/icons/index.ts
+++
@@ -1,8 +0,0 @@
-import ArrowDark from "./arrow-dark.svg";
-import Arrow from "./arrow.svg";
-import GithubDark from "./github-dark.svg";
-import Github from "./github.svg";
-import LinkedInDark from "./linkedin-dark.svg";
-import LinkedIn from "./linkedin.svg";
-
-export  { ArrowDark, Arrow, GithubDark, Github, LinkedInDark, LinkedIn }
---
+++ src/assets/index.ts
@@ -0,0 +1,28 @@
+import Resume from "./files/Maxwell Jones Resume.pdf";
+
+export { Resume };
+
+import Portrait from "./images/portrait.webp";
+
+export { Portrait };
+
+import ArrowDark from "../assets/icons/arrow-dark.svg";
+import Arrow from "../assets/icons/arrow.svg";
+import GithubDark from "../assets/icons/github-dark.svg";
+import Github from "../assets/icons/github.svg";
+import LinkedInDark from "../assets/icons/linkedin-dark.svg";
+import LinkedIn from "../assets/icons/linkedin.svg";
+
+export { ArrowDark, Arrow, GithubDark, Github, LinkedInDark, LinkedIn };
+
+import html from "../assets/icons/skills/html.svg";
+import css from "../assets/icons/skills/css.svg";
+import tailwind from "../assets/icons/skills/tailwind.svg";
+import sass from "../assets/icons/skills/sass.svg";
+import javascript from "../assets/icons/skills/javascript.svg";
+import react from "../assets/icons/skills/react.svg";
+import typescript from "../assets/icons/skills/typescript.svg";
+import php from "../assets/icons/skills/php.svg";
+import git from "../assets/icons/skills/git.svg";
+
+export { html, css, tailwind, sass, javascript, react, typescript, php, git };
