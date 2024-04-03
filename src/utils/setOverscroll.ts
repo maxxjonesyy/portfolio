04e1c83556b01deea6c1f7d96685f2fa0025e8a5
Max Jones <maxxjonesyy@gmail.com>
2024-04-03 17:29:00 +1100

    make overscroll black for dark theme

--- src/utils/setOverscroll.ts
+++ src/utils/setOverscroll.ts
@@ -1,12 +1,14 @@
 import { isDarkTheme } from "./index";
 
 function setOverScroll() {
-    const isDark = isDarkTheme();
-    const html = document.querySelector("html");
+  const isDark = isDarkTheme();
+  const html = document.querySelector("html");
 
-    if(html) {
-        isDark ? html.style.backgroundColor = '#00171F' : html.style.backgroundColor = '#FBFBFF';
-    }    
+  if (html) {
+    isDark
+      ? (html.style.backgroundColor = "#000000")
+      : (html.style.backgroundColor = "#FBFBFF");
+  }
 }
 
 export default setOverScroll;
