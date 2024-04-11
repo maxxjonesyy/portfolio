ae00400e68bf99ef9bc9120c3f2a3acf4ed593ea
Max Jones <maxxjonesyy@gmail.com>
2024-04-11 17:31:27 +1000

    remove vite chunking

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
-  build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-}
 });
