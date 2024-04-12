0034e351ed51c53bfccfc92a7c8d3500d73d0022
Maxwell Jones <maxxjonesyy@gmail.com>
2024-04-12 14:23:29 +1000

    UI changes for projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,7 @@ function ProjectCard({ project, setActiveProject }: ProjectCardType) {
 
   return (
     <div
-      className='grid grid-cols-1 md:grid-cols-2 items-center gap-5 transition-theme hover:background-glass text-body p-3 cursor-pointer'
+      className='grid grid-cols-1 md:grid-cols-2 items-center gap-5 text-body p-3 cursor-pointer'
       onClick={() => setActiveProject(project)}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}>
@@ -19,19 +19,22 @@ function ProjectCard({ project, setActiveProject }: ProjectCardType) {
         height={450}
       />
       <div>
+      
+        <div className={`p-5 transition-colors duration-300 ${hovered && 'background-glass'}`}>
         <h2
-          className={`text-xl font-medium flex gap-3 transition-theme ${
+          className={`text-xl font-medium flex gap-3 ${
             hovered && "text-accentText"
           }`}>
           {project.heading}
           <span
-            className={`-rotate-[130deg] transition-theme ${
+            className={`-rotate-[130deg] transition-transform duration-300 ${
               hovered && "-translate-y-1 text-accentText"
             }`}>
             &#8595;
           </span>
         </h2>
         <p className='my-5'>{project.description}</p>
+        
 
         <LinkButton
           download={false}
@@ -47,6 +50,7 @@ function ProjectCard({ project, setActiveProject }: ProjectCardType) {
           text='View Code'
           textSize='text-sm'
         />
+      </div>
       </div>
     </div>
   );
