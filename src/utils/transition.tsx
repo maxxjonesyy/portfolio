907778eeefc0751e8f84b6a3423b6eb6a16c5da4
Max Jones <maxxjonesyy@gmail.com>
2024-06-28 16:00:31 +1000

    change transition speed

--- src/utils/transition.tsx
+++ src/utils/transition.tsx
@@ -9,14 +9,14 @@ const transition = (Component: React.FC) => {
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
-        transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 0.7, ease: [0.22, 1, 0.36, 1] }}
       />
       <motion.div
         className='slide-out'
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 0.7, ease: [0.22, 1, 0.36, 1] }}
       />
     </>
   );
